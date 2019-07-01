fb67b2a8b9b4c5d8b7f5036dc610f4e6c1784f8d
Levt <Levit.igor01@gmail.com>
2019-07-01 22:13:53 +0300

    Homework for lesson5

--- Lecture_5/debounce.js
+++ Lecture_5/debounce.js
@@ -2,6 +2,27 @@
  * callback - функция которую нужно обернуть
  * time - время в миллисекундах
  */
+let timer = null;
 function debounce(callback, time) {
-  // Code
+    if (timer) {
+        clearTimeout(timer);
+    }
+    timer = setTimeout(callback, time);
 }
+
+let count = 0;
+function func() {
+    count++;
+    console.log('Function called', count, 'times');
+}
+
+debounce(func, 1000);
+debounce(func, 1000);
+
+setTimeout(function () {
+    debounce(func, 1000);
+}, 5100);
+setTimeout(function () {
+    debounce(func, 1000);
+}, 5900);
+
--- Lecture_5/throttle.js
+++ Lecture_5/throttle.js
@@ -3,5 +3,56 @@
  * time - время в миллисекундах
  */
 function throttle(callback, time) {
-  // Code
+
+    let isThrottled = false,
+        savedArgs,
+        savedThis;
+
+    function wrapper() {
+
+        if (isThrottled) { // (2)
+            savedArgs = arguments;
+            savedThis = this;
+            return;
+        }
+
+        callback.apply(this, arguments); // (1)
+
+        isThrottled = true;
+
+        setTimeout(function () {
+            isThrottled = false; // (3)
+            if (savedArgs) {
+                wrapper.apply(savedThis, savedArgs);
+                savedArgs = savedThis = null;
+            }
+        }, time);
+    }
+
+    return wrapper;
 }
+
+let count = 0;
+
+function func() {
+    count++;
+    console.log('Function called', count, 'times');
+}
+
+let tr = throttle(func, 1000);
+
+    setTimeout(() => {
+        tr();
+        tr();
+        tr();
+    }, 3100)
+
+tr();
+tr();
+tr();
+tr();
+tr();
+tr();
+tr();
+tr();
+tr();
