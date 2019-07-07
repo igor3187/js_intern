1efaedb04176d50b5f3e2e7baa0ac41f87c22115
I.Levit <Levit.igor01@gmail.com>
2019-07-07 22:33:08 +0300

    Homework for lesson6

--- Lecture_6/asyncAwait.js
+++ Lecture_6/asyncAwait.js
@@ -1,33 +1,47 @@
 const random = (max, min) => Math.floor(Math.random() * (max - min)) + min;
 
 function test() {
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => {
-      const user = data[random(10, 0)];
-      console.log('My user is', user);
-      return user;
-    })
-    .then(user => {
-      fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch('https://jsonplaceholder.typicode.com/users')
         .then(res => res.json())
         .then(data => {
-          const post = data[random(100, 0)];
-          console.log('My post is', post);
-          const finalRes = {
-            user,
-            post
-          };
-          console.log('Finally', finalRes);
+            const user = data[random(10, 0)];
+            console.log('My user is', user);
+            return user;
+        })
+        .then(user => {
+            fetch('https://jsonplaceholder.typicode.com/posts')
+                .then(res => res.json())
+                .then(data => {
+                    const post = data[random(100, 0)];
+                    console.log('My post is', post);
+                    const finalRes = {
+                        user,
+                        post
+                    };
+                    console.log('Finally', finalRes);
+                });
         });
-    });
 
-  console.log('I have to appear in a console first');
+    console.log('I have to appear in a console first');
 }
 
-function asyncTest() {
-  // implementation;
+async function asyncTest() {
+    console.log('I have to appear in a console first');
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    const user = data[random(10, 0)];
+    console.log('My user is', user);
+
+    const postRes = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const postData = await postRes.json();
+    const post = postData[random(100, 0)];
+    console.log('My post is', post);
+    const finalRes = {
+        user,
+        post
+    };
+    console.log(finalRes);
 }
 
-test();
+// test();
 asyncTest();
--- Lecture_6/ownFetch.js
+++ Lecture_6/ownFetch.js
@@ -1,10 +1,32 @@
-const ownFetch = () => {
-  // implementation
+const ownFetch = (URL) => {
+
+    return new Promise((resolve, reject) => {
+
+        const xhr = new XMLHttpRequest();
+
+        xhr.open('GET', URL, true);
+
+        xhr.send();
+
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+
+            if (this.status !== 200) {
+                const error = {
+                    code: this.status,
+                    message: this.statusText
+                };
+                reject(error);
+            }
+            const response = {
+                json: () => JSON.parse(this.responseText)
+            };
+            resolve(response);
+        };
+    });
 };
 
-
-
 ownFetch('https://jsonplaceholder.typicode.com/posts')
-  .then(console.log)
-  .then(res => res());
-  .catch(err => console.log(err));
+    .then(res => res.json())
+    .then(console.log)
+    .catch(err => console.log(err));
