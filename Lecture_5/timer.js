6b02f39840ea2f6fae177fdeeb7e4c1032480c87
I.Levit <Levit.igor01@gmail.com>
2019-07-07 22:31:17 +0300

    Homework for lesson5

--- Lecture_5/timer.js
+++ Lecture_5/timer.js
@@ -1,29 +1,58 @@
 class Timer {
-  constructor(initialTime) {
-    this.initialTime = initialTime;
-  }
+    constructor(initialTime) {
+        this.initialTime = this.toMs(initialTime);
+        this.timerId = null;
+    }
 
-  start() {
-    //
-  }
+    start() {
+        this.timerId = setInterval(() => {
+            this.initialTime -= 1000;
+            console.log('Seconds left: ' + this.toSeconds(this.initialTime));
+            if (!this.toSeconds(this.initialTime)) {
+                clearInterval(this.timerId);
+                this.timeOver();
+            }
+        },1000)
+    }
 
-  stop() {
-    //
-  }
+    stop() {
+        clearInterval(this.timerId);
+    }
 
-  reset() {
-    //
-  }
+    reset() {
+        this.initialTime = 0;
+    }
 
-  addTime(time) {
-    //
-  }
+    addTime(time) {
+        this.initialTime += this.toMs(time);
+        console.log('Added time', (time * 1000), 'Total', this.initialTime)
+    }
 
-  subtractTime(time) {
-    //
-  }
+    subtractTime(time) {
+        this.initialTime -= this.toMs(time);
+        console.log('Removed time', time, 'Total', this.initialTime)
+    }
 
-  timeOver() {
-    console.log("time over");
-  }
+    timeOver() {
+        console.log("time over");
+    }
+
+    toSeconds(time) {
+        return time / 1000;
+    }
+
+    toMs(time) {
+        return time * 1000;
+    }
 }
+
+let timer = new Timer(10);
+console.log(timer.start());
+setTimeout(() => {
+    timer.stop();
+    timer.addTime(2);
+}, 4000);
+setTimeout(() => {
+    timer.start();
+    timer.subtractTime(2);
+}, 6000);
