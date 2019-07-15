0dc16dd545b15cb2255e48cf32f1d6e7d3192b74
I.Levit <Levit.igor01@gmail.com>
2019-07-15 22:19:05 +0300

    Homework for lesson8

--- Lecture_8/prettify.js
+++ Lecture_8/prettify.js
@@ -3,7 +3,28 @@
 //   text
 //   <div>Bam</div>
 // </body>
-const elements = prettify(document);
+function prettify(document) {
+    let treeWalker = document.createTreeWalker(
+        document.body,
+        NodeFilter.SHOW_ELEMENT
+    );
+
+    while (treeWalker.nextNode()) {
+        const p = document.createElement('p');
+        const currentNode = treeWalker.currentNode;
+
+        if (currentNode.tagName === 'DIV') {
+            for (const node of currentNode.childNodes) {
+                if (node instanceof Text) {
+                    p.appendChild(node);
+                    currentNode.appendChild(p);
+                }
+            }
+        }
+    }
+}
+
+prettify(document);
 console.log(document.body.innerHTML);
 // <body>
 //   <p>Boom</p>
--- Lecture_8/recSearchTags.js
+++ Lecture_8/recSearchTags.js
@@ -3,6 +3,20 @@
 //   text
 //   <div><p>2</p></div>
 // </body>
+function recSearchTags(doc, tagName) {
+    let elements = [];
+    for (const element of doc.childNodes) {
+        if (element.tagName === tagName.toUpperCase()) {
+            elements.push(element)
+        }
+        if (element.hasChildNodes()) {
+            elements = elements.concat(recSearchTags(element, tagName));
+        }
+    }
+    return elements;
+}
+
 const elements = recSearchTags(document, 'p');
 // ['<p>1</p>' '<p>2</p>'] где каждый элемент это объект соответствующего типа
 console.log(elements.length); // 2
+console.log(elements);
