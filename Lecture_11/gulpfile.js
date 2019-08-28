70da3fbbb3783e24bb0014c70ff71124143df212
I.Levit <Levit.igor01@gmail.com>
2019-08-28 19:19:58 +0300

    Homework for lesson11

--- Lecture_11/gulpfile.js
+++ Lecture_11/gulpfile.js
@@ -1,35 +1,28 @@
 const gulp = require('gulp');
-const htmlmin = require('gulp-htmlmin');
+const htmlMin = require('gulp-htmlmin');
 const sass = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
-const uglifycss = require('gulp-uglifycss');
+const cleanCss = require('gulp-clean-css');
 
-gulp.task('css',  async function () {
-    gulp.src('./dist/*.css')
-        .pipe(uglifycss({
-            "maxLineLen": 80,
-            "uglyComments": true
-        }))
-        .pipe(gulp.dest('./dist/'));
+gulp.task('run', ['html', 'scss']);
+
+gulp.task('watch', function () {
+    gulp.watch('./src/*.html', ['html']);
+    gulp.watch('./src/*.scss', ['scss']);
 });
-// With this task('minify-css') nothing was changed inside of './dist/style/css', so i use a task 'gulp-uglifycss'
-gulp.task('minify-css', () => {
-    return gulp.src('styles/*.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('dist'));
-});
+
+gulp.task('default', ['run', 'watch']);
 
 sass.compiler = require('node-sass');
 
-gulp.task('sass', function () {
+gulp.task('scss', function () {
     return gulp.src('./src/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCss({compatibility: 'ie8'}))
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', () => {
-    return gulp.src('index/*.html')
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest('dist'));
-
+gulp.task('html', () => {
+    return gulp.src('./src/*.html')
+        .pipe(htmlMin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./dist'));
 });
